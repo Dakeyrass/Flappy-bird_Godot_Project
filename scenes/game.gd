0c2165d9d68748366c700f9467c10cd4b3fab698
Dakeyrass <pwkmarion@gmail.com>
2026-01-11 21:48:50 +0100

    entirely reworked script + added ground

---
+++ scenes/game.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+var game_running : bool
+var game_over : bool
+var scroll
+var score
+const SCROLL_SPEED : int = 4
+var screen_size: Vector2i
+var ground_height : int
+var pipes : Array
+const PIPE_DELAY : int = 100
+const PIPE_RANGE : int = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_window().size
+	new_game()
+	
+func new_game():
+	#reset variables
+	game_running = false
+	game_over = false
+	score = 0
+	scroll = 0
+	$player.reset()
+	
+func _input(event):
+	if game_over == false:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				if game_running == false:
+					start_game()
+				else:
+					if $player.flying:
+						$player.flap()
+ 
+func start_game():
+	game_running = true
+	$player.flying = true
+	$player.flap()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if game_running:
+		scroll += SCROLL_SPEED
+		if scroll >= screen_size.x:
+			scroll = 0
+		$ground.position.x = -scroll
