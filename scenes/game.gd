70edc4afad83fe9220ecbe4a9fc4c61761483157
Dakeyrass <pwkmarion@gmail.com>
2026-01-12 18:37:36 +0100

    added comments + build

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+#visible et modifiable dans l’inspecteur/stock la scene dans une var accessible
 @export var pipe_scene : PackedScene
 
 var game_running : bool
@@ -11,7 +12,7 @@ var screen_size: Vector2i
 var ground_height : int
 var pipes : Array
 const PIPE_DELAY : int = 100
-const PIPE_RANGE : int = 200
+const PIPE_RANGE : int = 200 #valeur maximale du décalage vertical aléatoire
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,8 +26,8 @@ func new_game():
 	game_over = false
 	score = 0
 	$scoreLabel.text = "SCORE " + str(score)
-	$gameOver.hide()
-	get_tree().call_group("pipes", "queue_free")
+	$gameOver.hide() #cache le bouton reset (ici cache directement la scene contenant le bouton)
+	get_tree().call_group("pipes", "queue_free") #Supprime proprement tous les nodes du groupe pipes en une seule commande
 	scroll = 0
 	pipes.clear()
 	generate_pipes()
@@ -54,18 +55,18 @@ func _process(delta: float) -> void:
 	if game_running:
 		scroll += SCROLL_SPEED
 		if scroll >= screen_size.x:
-			scroll = 0
-		$ground.position.x = -scroll
+			scroll = 0 #quand décalage atteint la largeur de l’écran on remet scroll à 0
+		$ground.position.x = -scroll #décale le sol vers la gauche
 		for pipe in pipes:
-			pipe.position.x -= SCROLL_SPEED
+			pipe.position.x -= SCROLL_SPEED  #déplace chaque tuyau vers la gauche à la même vitesse que le sol
 
 func _on_pipe_timer_timeout() -> void:
 	generate_pipes()
 	
 func generate_pipes():
-	var pipe = pipe_scene.instantiate()
+	var pipe = pipe_scene.instantiate() #= node vivant manipulable
 	pipe.position.x = screen_size.x + PIPE_DELAY
-	pipe.position.y = (screen_size.y - ground_height) / 2 + randi_range(-PIPE_RANGE, PIPE_RANGE)
+	pipe.position.y = (screen_size.y - ground_height) / 2 + randi_range(-PIPE_RANGE, PIPE_RANGE) #centre verticalement + décalage aléatoire pour varier hauteur tuyau
 	pipe.hit.connect(player_hit)
 	pipe.scored.connect(scored)
 	add_child(pipe)
--- scenes/pipe.gd
+++ scenes/pipe.gd
@@ -3,8 +3,8 @@ extends Area2D
 signal hit 
 signal scored
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	hit.emit() # Quand quelque chose touche cette Area2D, envoie le signal "hit"
 
-func _on_score_area_body_entered(body: Node2D) -> void:
+func _on_score_area_body_entered(_body: Node2D) -> void:
 	scored.emit()
